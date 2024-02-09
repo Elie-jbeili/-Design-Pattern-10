ec969e251e1fea2fce555c418b092ded44fb66ef
Elie-jbeili <117976718+Elie-jbeili@users.noreply.github.com>
2024-02-09 23:26:05 +0100

    CsvFileHandler.java

--- src/main/java/com/fges/todoapp/CsvFileHandler.java
+++ src/main/java/com/fges/todoapp/CsvFileHandler.java
@@ -1,62 +1,24 @@
 package com.fges.todoapp;
 
-import org.apache.commons.cli.*;
-
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
 
-public class App {
+public class CsvFileHandler implements FileHandler {
+    private final Path filePath;
+    private final FileAccess fileAccess;
 
-    public static void main(String[] args) throws Exception {
-        System.exit(exec(args));
+    public CsvFileHandler(Path filePath, FileAccess fileAccess) {
+        this.filePath = filePath;
+        this.fileAccess = fileAccess;
     }
 
-    public static int exec(String[] args) throws IOException {
-        Options cliOptions = new Options();
-        cliOptions.addRequiredOption("s", "source", true, "File containing the todos");
-        cliOptions.addOption("d", "done", false, "Mark the todo as done");
+    @Override
+    public void insert(Todo todo) throws IOException {
+        // Implement CSV insertion logic
+    }
 
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd;
-        try {
-            cmd = parser.parse(cliOptions, args);
-        } catch (ParseException ex) {
-            System.err.println("Fail to parse arguments: " + ex.getMessage());
-            return 1;
-        }
-
-        String fileName = cmd.getOptionValue("s");
-        boolean isDone = cmd.hasOption("done");
-        List<String> positionalArgs = cmd.getArgList();
-        if (positionalArgs.isEmpty()) {
-            System.err.println("Missing Command");
-            return 1;
-        }
-
-        FileAccess fileAccess = new SimpleFileAccess();
-        FileHandler fileHandler = FileHandlerFactory.getFileHandler(fileName, fileAccess);
-
-        String command = positionalArgs.get(0);
-        switch (command) {
-            case "insert":
-                if (positionalArgs.size() < 2) {
-                    System.err.println("Missing TODO name");
-                    return 1;
-                }
-                String todoName = positionalArgs.get(1);
-                Todo todo = new Todo(todoName, isDone);
-                fileHandler.insert(todo);
-                break;
-            case "list":
-                fileHandler.list(isDone);
-                break;
-            default:
-                System.err.println("Unknown Command");
-                return 1;
-        }
-
-        System.err.println("Done.");
-        return 0;
+    @Override
+    public void list(boolean onlyDone) throws IOException {
+        // Implement CSV listing logic
     }
 }
